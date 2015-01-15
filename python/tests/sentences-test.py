4e3a75e29e2022d0705e5b710abdc8ee8c604dcc
Bernard Greenberg <bsg@basistech.com>
2015-01-15 15:17:29 -0500

    RCB-2 test_morphology now checks lemmas and parts of speech successfully.

--- python/tests/sentences-test.py
+++ python/tests/sentences-test.py
@@ -23,49 +23,41 @@ import json
 logging.basicConfig(level=logging.DEBUG)
 
 class SentencesTestCase(unittest.TestCase):
- 
-    def test_sentence_splitting(self):
+    def __init__(self, tcname):
+        super(SentencesTestCase,self).__init__(tcname)
         port = os.environ['MOCK_SERVICE_PORT']
-        url = 'http://localhost:' + port + '/raas'
-        url = "http://jugmaster.basistech.net/rest/v1"
-        logging.info("URL " + url)
+        self.lurl = 'http://localhost:' + port + '/raas'
         params = RaasParameters()
         params.content = "Yes, Ma'm! Green eggs and ham?  I am Sam;  I filter Spam."
         params.contentType = "text/plain"
         params.unit = "doc"
-        # the mock services can't respond to the individual params.
-        op = API(service_url = url).sentences_split()
-        result = op.operate(params, None)
+        self.HamParams = params
+
+    def getBaseURL(self):
+        url = self.lurl
+        url = "http://jugmaster.basistech.net/rest/v1"
+        logging.info("URL " + url)
+        return url
+        
+    def test_sentence_splitting(self):
+        op = API(service_url = self.getBaseURL()).sentences_split()
+        result = op.operate(self.HamParams, None)
         self.assertIsNotNone(result['sentences'])
         self.assertEqual(len(result['sentences']), 3)
 
     def test_tokenizing(self):
-        port = os.environ['MOCK_SERVICE_PORT']
-        url = 'http://localhost:' + port + '/raas'
-        url = "http://jugmaster.basistech.net/rest/v1"
-        logging.info("URL " + url)
-        params = RaasParameters()
-        params.content = "Yes, Ma'm! Green eggs and ham?  I am Sam;  I filter Spam."
-        params.contentType = "text/plain"
-        params.unit = "doc"
-
-        op = API(service_url = url).tokenize()
-        result = op.operate(params, None)
+        op = API(service_url = self.getBaseURL()).tokenize()
+        result = op.operate(self.HamParams, None)
         self.assertEqual(len(result['tokens']), 18)
 
     def test_morphology(self):
-        port = os.environ['MOCK_SERVICE_PORT']
-        url = 'http://localhost:' + port + '/raas'
-        url = "http://jugmaster.basistech.net/rest/v1"
-        logging.info("URL " + url)
-        params = RaasParameters()
-        params.content = "Yes, Ma'm! Green eggs and ham?  I am Sam;  I filter Spam."
-        params.contentType = "text/plain"
-        params.unit = "doc"
-
-        op = API(service_url = url).morphology("part-of-speech,lemmas")
-        result = op.operate(params, None)
-        print >>sys.stderr, result
-
+        op = API(service_url = self.getBaseURL()).morphology("parts-of-speech")
+        result = op.operate(self.HamParams, None)
+        presult = [x['pos'] for x in result['posTags']]
+        self.assertEqual(presult, [u'NOUN', u'CM', u'NOUN', u'VBPRES', u'SENT', u'ADJ', u'NOUN', u'COORD', u'NOUN', u'SENT', u'PRONPERS', u'VBPRES', u'PROP', u'SENT', u'PRONPERS', u'VI', u'PROP', u'SENT'])
+        op = API(service_url = self.getBaseURL()).morphology("lemmas")
+        result = op.operate(self.HamParams, None)
+        lresult = [x['lemma'] for x in result['lemmas']]
+        self.assertEqual(lresult, [u'yes', u',', u'ma', u'be', u'!', u'green', u'egg', u'and', u'ham', u'?', u'I', u'be', u'Sam', u';', u'I', u'filter', u'Spam', u'.'])
 
         
