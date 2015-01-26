f1e83db1c2c62fa957e1fea9217f1fc935a90ad2
Bernard Greenberg <bsg@basistech.com>
2015-01-26 12:28:20 -0500

    RCB-2 Move URL to pom. Don't kill mockserver run yet.

--- python/tests/api-test.py
+++ python/tests/api-test.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 """
  ** This data and information is proprietary to, and a valuable trade secret
  ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
@@ -24,11 +23,23 @@ import json
 
 logging.basicConfig(level=logging.DEBUG)
 
+
+
 class APITestCase(unittest.TestCase):
     def __init__(self, tcname):
         super(APITestCase,self).__init__(tcname)
-        port = os.environ['MOCK_SERVICE_PORT']
-        self.lurl = 'http://localhost:' + port + '/raas'
+        burl = os.environ['RAAS_SERVICE_URL']
+        if burl is not None:
+            burl = burl.strip()
+            if burl == "":
+                burl = None
+        if burl is None:
+            port = os.environ['MOCK_SERVICE_PORT']
+            self.url = 'http://localhost:' + port + '/raas'
+        else:
+            self.url = burl
+        logging.info("URL " + self.url)
+        self.api = API(service_url = self.url)
         params = RaasParameters()
         params.content = "Yes, Ma'm! Green eggs and ham?  I am Sam;  I filter Spam."
         params.contentType = DataFormat.SIMPLE
@@ -40,27 +51,18 @@ class APITestCase(unittest.TestCase):
         params.unit = InputUnit.DOC
         self.TagParams = params
 
-    def getBaseURL(self):
-        url = self.lurl
-        url = "http://jugmaster.basistech.net/rest/v1"
-        logging.info("URL " + url)
-        return url
-
-    def getAPI(self):
-        return API(service_url = self.getBaseURL())
-
     def test_ping(self):
-        op = self.getAPI().pinger()
+        op = self.api.pinger()
         result = op.ping()
         self.assertEqual(result['message'],'RaaS at your service')
 
     def test_language_info(self):
-        op = self.getAPI().language_detection()
+        op = self.api.language_detection()
         result = op.getInfo(None)
         # not much to do right now.
 
     def test_adm_detection(self):
-        op = self.getAPI().language_detection()
+        op = self.api.language_detection()
         result = op.operate(self.HamParams, ResultFormat.ROSETTE)
 
         self.assertIsNotNone(result['requestId'])
@@ -71,46 +73,46 @@ class APITestCase(unittest.TestCase):
 
 
     def test_sentence_splitting(self):
-        op = self.getAPI().sentences_split()
+        op = self.api.sentences_split()
         result = op.operate(self.HamParams, None)
         self.assertIsNotNone(result['sentences'])
         self.assertEqual(len(result['sentences']), 3)
 
     def test_tokenizing(self):
-        op = self.getAPI().tokenize()
+        op = self.api.tokenize()
         result = op.operate(self.HamParams, None)
         self.assertEqual(len(result['tokens']), 18)
 
     def test_morphology(self):
-        op = self.getAPI().morphology(MorphologyOutput.PARTS_OF_SPEECH)
+        op = self.api.morphology(MorphologyOutput.PARTS_OF_SPEECH)
         result = op.operate(self.HamParams, None)
         presult = [x['pos'] for x in result['posTags']]
         self.assertEqual(presult, [u'NOUN', u'CM', u'NOUN', u'VBPRES', u'SENT', u'ADJ', u'NOUN', u'COORD', u'NOUN', u'SENT', u'PRONPERS', u'VBPRES', u'PROP', u'SENT', u'PRONPERS', u'VI', u'PROP', u'SENT'])
-        op = API(service_url = self.getBaseURL()).morphology(MorphologyOutput.LEMMAS)
+        op = self.api.morphology(MorphologyOutput.LEMMAS)
         result = op.operate(self.HamParams, None)
         lresult = [x['lemma'] for x in result['lemmas']]
         self.assertEqual(lresult, [u'yes', u',', u'ma', u'be', u'!', u'green', u'egg', u'and', u'ham', u'?', u'I', u'be', u'Sam', u';', u'I', u'filter', u'Spam', u'.'])
 
     def test_entities(self):
-        op = self.getAPI().entities(None); # "linked" flag
+        op = self.api.entities(None); # "linked" flag
         result = op.operate(self.TagParams, None)
         # Not the right answer, but what it currently gets.
         self.assertEquals(len(result['entities']), 3)
 
     def test_categories(self):
-        op = self.getAPI().categories();
+        op = self.api.categories();
         result = op.operate(self.HamParams, None)
         self.assertEquals(len(result['categories']), 1)
 
     def test_sentiment(self):
-        op = self.getAPI().sentiment();
+        op = self.api.sentiment();
         result = op.operate(self.HamParams, None)
         ary = result['sentiment']
         sary = sorted(ary, key=lambda x: -x['confidence'])
         self.assertEqual(sary[0]['label'], "pos")
 
     def test_translate_name(self):
-        op = self.getAPI().translate_name();
+        op = self.api.translate_name();
         params = RntParameters()
         params.name = "كريم عبد الجبار"
         params.entityType = "PERSON";
