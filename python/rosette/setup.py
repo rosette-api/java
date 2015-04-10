b8ea9f2ac4fd2520e24a64833c5719851a0d7214
Bernard Greenberg <bsg@basistech.com>
2015-04-10 10:23:32 -0400

    RCB-11 New setup.py in user space/terms.

---
+++ python/rosette/setup.py
@@ -0,0 +1,21 @@
+NAME = "Rosette"
+DESCRIPTION = "Python client binding for Basis Technology Rosette web service"
+AUTHOR = "Basis Techology Corp."
+HOMEPAGE = "http://www.basistech.com"
+PLATFORMS = "Python 2.7 and Python 3.4/later"
+
+from setuptools import setup
+setup(name=NAME,
+      author=AUTHOR,
+      description=DESCRIPTION,
+      license="Python (MIT Style)",
+      long_description=DESCRIPTION,
+      package_dir={
+        'rosette': '.'
+      },
+      packages=['rosette'],
+      platforms=PLATFORMS,
+      url=HOMEPAGE,
+      version='1.0',
+
+      )
