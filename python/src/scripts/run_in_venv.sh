dc7bc9169ea8f5e2f7f6bb720d72f20ae52d0d0e
Bernard Greenberg <bsg@basistech.com>
2015-04-11 10:53:53 -0400

    RCB-11 Much cooler in-mvn way found.  Unkludge.

--- python/src/scripts/run_in_venv.sh
+++ python/src/scripts/run_in_venv.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#  Setups.py have to execute in a give directory; not convenient from mvn.
-if [ $1 == --cwd ] ; then
-   cd $2
-   shift
-   shift
-fi
 venv=$1
 shift
 . $venv/bin/activate
