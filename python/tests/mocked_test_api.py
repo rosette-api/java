c7c39f8cdb610461d5326ffa937582eed840b3c0
rhausmann <rhausmann@basistech.com>
2015-06-15 09:13:37 -0400

    RCB-44: Fixed style issues, code consolidation

--- python/tests/mocked_test_api.py
+++ python/tests/mocked_test_api.py
@@ -1,305 +1,172 @@
 # -*- coding: utf-8 -*-
 
-# To run tests, run `py.test python/tests/mocked_tes_api.py` from the ws-client-bindings directory
-# Must be run from this level in order to access the correct file paths for test files
+"""
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2015 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+"""
+
+# To run tests, run `py.test mocked_test_api.py`
 
 import httpretty
 import json
 import logging
 import os
 import pytest
-from rosette.api import API, Operator, RosetteParameters, RntParameters, DataFormat, MorphologyOutput, RniParameters, RosetteException
-import sys
-_IsPy3 = sys.version_info[0] == 3
+import re
+from rosette.api import API, Operator, RosetteParameters, RntParameters, RniParameters, RosetteException
 
-try:
-    from urlparse import urlparse
-except ImportError:
-    from urllib.parse import urlparse
-try:
-    import httplib
-except ImportError:
-    import http.client as httplib
 
 # Set up mocking
 def mock_rosette_api():
-
     # For post requests, define what to return
     def request_callback(request, uri, headers):
+        # Filename is in the user_key field as a workaround as the mocked tests don't require a user key
         filename = request.headers["user_key"]
-        resp_file = 'mock-data/response/'+filename
-        status_file = open(resp_file+'.status')
-        status = status_file.read()
-        status_file.close()
-        resp_json_file = open(resp_file+'.json')
-        resp = resp_json_file.read()
-        resp_json_file.close()
+        # Construct the base name for the response file from the filename
+        # Example filename: eng-doc-categories
+        resp_file = os.path.dirname(__file__) + "/../../mock-data/response/" + filename
+        # Retrieve data from the corresponding status file
+        with open(resp_file + ".status", "r") as status_file:
+            status = status_file.read()
+        # Retrieve data from corresponding json file
+        with open(resp_file + ".json", "r") as resp_json_file:
+            resp = resp_json_file.read()
         return int(status), headers, resp
 
     # Register mocked URLs with HTTPretty
-    for end in ['categories', 'entities', 'entities/linked', 'language', 'matched-name', 'morphology/complete',
-                'morphology/compound-components', 'morphology/han-readings', 'morphology/lemmas',
-                'morphology/parts-of-speech', 'sentences', 'sentiment', 'tokens', 'translated-name']:
-        httpretty.register_uri(
-                httpretty.POST, "https://api.rosette.com/rest/v1/"+end,
-                body=request_callback)
+    # All post requests dynamically respond based on request
+    for end in ["categories", "entities", "entities/linked", "language", "matched-name", "morphology/complete",
+                "morphology/compound-components", "morphology/han-readings", "morphology/lemmas",
+                "morphology/parts-of-speech", "sentences", "sentiment", "tokens", "translated-name"]:
+        httpretty.register_uri(httpretty.POST, "https://api.rosette.com/rest/v1/" + end, body=request_callback)
 
-    info_file = open('mock-data/response/info.json')
-    httpretty.register_uri(httpretty.GET, "https://api.rosette.com/rest/v1/info", body=info_file.read(), status=200)
-    info_file.close()
+    # Get requests (info and ping) always have the same response because there is no request body
+    with open(os.path.dirname(__file__) + "/../../mock-data/response/info.json", "r") as info_file:
+        body = info_file.read()
+    httpretty.register_uri(httpretty.GET, "https://api.rosette.com/rest/v1/info", body=body, status=200)
 
-    ping_file = open('mock-data/response/ping.json')
-    httpretty.register_uri(httpretty.GET, "https://api.rosette.com/rest/v1/ping", body=ping_file.read(), status=200)
-    ping_file.close()
+    with open(os.path.dirname(__file__) + "/../../mock-data/response/ping.json", "r") as ping_file:
+        body = ping_file.read()
+    httpretty.register_uri(httpretty.GET, "https://api.rosette.com/rest/v1/ping", body=body, status=200)
+
 
 # Set up app based on request file name
 class App:
     def __init__(self, filename):
         self.url = "https://api.rosette.com/rest/v1"
-        # Set user key as filename as a workaround - tests don't require user key
+        # Set user key as filename as a workaround - tests don"t require user key
+        # Filename is necessary to get the correct response in the mocked test
         key = filename
         logging.info("URL " + self.url)
         self.api = API(service_url=self.url, user_key=key)
+        # Default to RosetteParameters as self.params
+        self.params = RosetteParameters()
+        # Name matching endpoint requires RniParameters
         if "matched-name" in filename:
             self.params = RniParameters()
-            f = open("mock-data/request/"+filename+".json")
-            paramsDict = json.loads(f.read())
-            for key in paramsDict:
-                self.params[key] = paramsDict[key]
-            f.close()
+        # Name translation requires RntParameters
         elif "translated-name" in filename:
             self.params = RntParameters()
-            f = open("mock-data/request/"+filename+".json")
-            paramsDict = json.loads(f.read())
-            for key in paramsDict:
-                self.params[key] = paramsDict[key]
-            f.close()
-        elif filename != '':
-            self.params = RosetteParameters()
-            f = open("mock-data/request/"+filename+".json")
-            paramsDict = json.loads(f.read())
-            for key in paramsDict:
-                self.params[key] = paramsDict[key]
-            f.close()
+        # Filename is only "" if it is a ping or info test -- they do not have request files
+        if filename != "":
+            # Find and load contents of request file into parameters
+            with open(os.path.dirname(__file__) + "/../../mock-data/request/" + filename + ".json", "r") as inp_file:
+                params_dict = json.loads(inp_file.read())
+            for key in params_dict:
+                self.params[key] = params_dict[key]
 
+
+# Run through all files in the mock-data directory, extract endpoint, and create a list of tuples of the form
+# (input filename, output filename, endpoint) as the elements
+def categorize_reqs():
+    # Define the regex pattern of file names. Example: eng-doc-categories.json
+    pattern = re.compile('(\w+-\w+-([a-z_-]+))[.]json')
+    files = []
+    # Loop through all file names in the mock-data/request directory
+    for filename in os.listdir(os.path.dirname(__file__) + "/../../mock-data/request"):
+        # Extract the endpoint (the part after the first two "-" but before .json
+        endpt = pattern.match(filename).group(2)
+        # Add (input, output, endpoint) to files list
+        files.append((pattern.match(filename).group(1), os.path.dirname(__file__) + "/../../mock-data/response/" +
+                      filename, endpt))
+    return files
+
+# Setup for tests - register urls with HTTPretty and compile a list of all necessary information about each file
+# in mock-data/request so that tests can be run
+mock_rosette_api()
+docs_list = categorize_reqs()
+
+
+# Test that pinging the API is working properly
 def test_ping():
-    app = App('')
+    app = App("")
+    # HTTPretty intercepts calls while enabled
     httpretty.enable()
-    mock_rosette_api()
     op = app.api.ping()
     result = op.ping()
     httpretty.disable()
-    assert result['message'] == 'Rosette API at your service'
+    assert result["message"] == "Rosette API at your service"
 
+
+# Test that getting the info about the API is being called correctly
 def test_info():
-    app = App('')
+    app = App("")
     httpretty.enable()
-    mock_rosette_api()
     op = Operator(app.api, None)
     result = op.info()
     httpretty.disable()
     assert result["buildNumber"] == "6bafb29d"
     assert result["name"] == "Rosette API"
 
-def categorizeReqs():
-    docs = {}
-    limiter = 0
-    for end in ['categories', 'entities', 'entities_linked', 'language', 'matched-name', 'morphology_complete',
-                'morphology_compound-components', 'morphology_han-readings', 'morphology_lemmas',
-                'morphology_parts-of-speech', 'sentences', 'sentiment', 'tokens', 'translated-name']:
-        docs[end] = []
-    for filename in os.listdir("mock-data/request"):
-        if "morphology" not in filename and "name" not in filename:
-            limiter += 1
-            endpt = filename.split(".")[0].split("-")[-1]
-            docs[endpt].append((filename.split(".")[0], "mock-data/response/"+filename))
-        elif "name" in filename:
-            if "matched" in filename:
-                endpt = "matched-name"
-            else:
-                endpt = "translated-name"
-            docs[endpt].append((filename.split(".")[0], "mock-data/response/"+filename))
-        elif "morphology" in filename:
-            facet = filename.split("morphology")[-1].split(".")[0]
-            docs["morphology"+facet].append((filename.split(".")[0], "mock-data/response/"+filename))
-    return docs
 
-docs = categorizeReqs()
-
-@pytest.mark.parametrize("input,expected", docs["categories"])
-def test_categories(input, expected):
+# Test all other endpoints
+# docs_list is the list of information from documents in the mock-data/request directory above
+# @pytest.mark.parametrize("inp, expected, endpt", docs_list) means that it will call the below test for each tuple
+# in the docs_list feeding the elements of the tuple as arguments to the test
+@pytest.mark.parametrize("inp, expected, endpt", docs_list)
+def test_all(inp, expected, endpt):
     error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
+    # Create an instance of the app, feeding the filename to be stored as the user key so the response will be correct
+    app = App(inp)
+    # Open the expected response file and store the data
+    with open(expected, "r") as expected_file:
+        res = json.loads(expected_file.read())
+    # Check to see if this particular request should throw an exception for an unsupported language
     if "code" in res:
         if res["code"] == "unsupportedLanguage":
             error = True
     httpretty.enable()
-    op = app.api.categories()
+    functions = {"categories": app.api.categories(),
+                 "entities": app.api.entities(None),
+                 "entities_linked": app.api.entities(True),
+                 "language": app.api.language(),
+                 "matched-name": app.api.matched_name(),
+                 "morphology_complete": app.api.morphology(),
+                 "sentiment": app.api.sentiment(),
+                 "translated-name": app.api.translated_name()}
+    # Find the correct function based on the endpoint and create an operator
+    op = functions[endpt]
+    # If the request is expected to throw an exception, try complete the operation and pass the test only if it fails
     if error:
         try:
-            result = op.operate(app.params)
+            op.operate(app.params)
             assert False
         except RosetteException:
             assert True
             return
+    # Otherwise, actually complete the operation and check that it got the correct result
     result = op.operate(app.params)
     httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["sentiment"])
-def test_sentiment(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.sentiment()
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["language"])
-def test_language(input, expected):
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    httpretty.enable()
-    op = app.api.language()
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["entities"])
-def test_entities(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.entities(None)
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            print input
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["entities_linked"])
-def test_entities_linked(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.entities(True)
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            print input
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["morphology_complete"])
-def test_morphology_complete(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.morphology()
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            print input
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["matched-name"])
-def test_matched_name(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.matched_name()
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            print input
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
-
-@pytest.mark.parametrize("input, expected", docs["translated-name"])
-def test_translated_name(input, expected):
-    error = False
-    app = App(input)
-    f = open(expected)
-    res = json.loads(f.read())
-    f.close()
-    if "code" in res:
-        if res["code"] == "unsupportedLanguage":
-            error = True
-    httpretty.enable()
-    op = app.api.translated_name()
-    if error:
-        try:
-            result = op.operate(app.params)
-            assert False
-        except RosetteException:
-            print input
-            assert True
-            return
-    result = op.operate(app.params)
-    httpretty.disable()
-    assert result == res
+    if result == res:
+        assert True
+    else:
+        assert False
