085fcc43a17b11fb2b0c2f97ef58abdc3a5ba3d6
Li Xu <lxu@basistech.com>
2015-07-05 15:12:56 -0400

    RCB-65,RCB-66: fix style for examples too

--- php/examples/morphology_compound-components.php
+++ php/examples/morphology_compound-components.php
@@ -1,26 +1,26 @@
 <?php
-/**
- * Example code to call Rosette API to get de-compounded words from a piece of text
- **/
 
-require_once(dirname(__FILE__) . "/../vendor/autoload.php");    // assuming composer.json is properly configured with Rosette API
+/**
+ * Example code to call Rosette API to get de-compounded words from a piece of text.
+ **/
+require_once dirname(__FILE__).'/../vendor/autoload.php';    // assuming composer.json is properly configured with Rosette API
 use rosette\api\Api;
 use rosette\api\DocumentParameters;
 use rosette\api\RosetteConstants;
 use rosette\api\RosetteException;
 
-$options = getopt(null, array("key:", "url::"));
-if (!isset($options["key"])) {
-    echo "Usage: php " . __FILE__ . " --key <api_key> --url=<alternate_url>\n";
+$options = getopt(null, array('key:', 'url::'));
+if (!isset($options['key'])) {
+    echo 'Usage: php '.__FILE__." --key <api_key> --url=<alternate_url>\n";
     exit();
 }
 
-$api = isset($options["url"]) ? new Api($options["key"], $options["url"]) : new Api($options["key"]);
+$api = isset($options['url']) ? new Api($options['key'], $options['url']) : new Api($options['key']);
 $params = new DocumentParameters();
-$params->set("content", "Rechtsschutzversicherungsgesellschaften");
+$params->set('content', 'Rechtsschutzversicherungsgesellschaften');
 
 try {
-    $result = $api->morphology($params, RosetteConstants::$MorphologyOutput["COMPOUND_COMPONENTS"]);
+    $result = $api->morphology($params, RosetteConstants::$MorphologyOutput['COMPOUND_COMPONENTS']);
     var_dump($result);
 } catch (RosetteException $e) {
     error_log($e);
