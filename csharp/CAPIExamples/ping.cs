a374e0b270aa0247b3730ef8fc5ae055e04a0c56
William Spitzer <wspitzer@win-wspit-4838.basistech.net>
2015-07-22 16:45:27 -0400

    modified examples

--- csharp/CAPIExamples/ping.cs
+++ csharp/CAPIExamples/ping.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CBinding;
 using System.Web.Script.Serialization;
+using rosette_api;
 
 namespace CAPIExamples
 {
