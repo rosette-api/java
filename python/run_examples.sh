6585b290f9a0f92590ca23355d30b3c913c1431a
rhausmann <rhausmann@basistech.com>
2015-08-18 14:55:24 -0400

    RCB-116: Auto-download rosette_api from pip

--- python/run_examples.sh
+++ python/run_examples.sh
@@ -2,7 +2,14 @@
 
 mvn clean install
 cd target/github-publish/examples
+echo "Enter your password to allow sudo to pip install rosette_api"
+sudo pip install rosette_api
 for f in *.py
 do
     python $f --key $1
 done
+
+echo "Uninstalling rosette_api"
+sudo pip uninstall rosette_api
+echo "Logged out of sudo"
+sudo -k
