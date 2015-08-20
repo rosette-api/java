44b1344be84174d2505c01e9bff2b83baaab0b84
rhausmann <rhausmann@basistech.com>
2015-08-20 15:06:56 -0400

    RCB-116: Add run all

---
+++ php/run_examples.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash -e
+
+mvn clean install
+cd target/github-publish/examples
+for f in *.php
+do
+    php $f --key $1
+done
---
+++ run_all_examples.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash -e
+
+bindings=(java nodejs php python)
+key=$1
+
+for i in ${bindings[@]}
+do
+    cd $i
+    ./run_examples.sh $key
+    cd ..
+done
