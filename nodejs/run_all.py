9559b7dd6751d09370d2ebc13b231ab076ae671c
William Spitzer <wspitzer@basistech.com>
2015-08-28 13:36:02 -0400

    updated run_all.py nodejs

--- nodejs/run_all.py
+++ nodejs/run_all.py
@@ -75,7 +75,7 @@ for f in listdir(os.path.realpath('.')):
         except:
             print f + " was unable to be compiled and run"
             failures = failures + [f]
-        time.sleep(1)
+        time.sleep(2)
 
 # Exit test folder
 try:
