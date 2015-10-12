b5ab5ae1332e2d27c89aaf45aa1cce50c60fc7eb
William Spitzer <wspitzer@basistech.com>
2015-10-12 01:37:05 -0400

    Fixed whitespace issues

--- python/examples/entities.py
+++ python/examples/entities.py
@@ -12,7 +12,7 @@ from rosette.api import API, DocumentParameters
 def run(key):
     # Create an API instance
     api = API(user_key=key)
-    
+
     params = DocumentParameters()
     params["content"] = u"${entities_data}"
     result = api.entities(params)  # entity linking is turned off
