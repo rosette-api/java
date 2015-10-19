af4c0302f9615b6c812507d3c11dcc41a7501842
William Spitzer <wspitzer@basistech.com>
2015-10-19 00:35:27 -0400

    Updated publish/pom files for ruby/shell. Added interpolate.py code to handle ruby/shell examples and move them into the index.md. Also changed shell from -H' to -H '.

--- shell/examples/info.sh
+++ shell/examples/info.sh
@@ -1,1 +1,1 @@
-curl "https://api.rosette.com/rest/v1/info" -H'user_key: [your_api-key]'
+curl "https://api.rosette.com/rest/v1/info" -H 'user_key: [your_api-key]'
--- shell/examples/ping.sh
+++ shell/examples/ping.sh
@@ -1,1 +1,1 @@
-curl "https://api.rosette.com/rest/v1/ping" -H'user_key: [your_api-key]'
+curl "https://api.rosette.com/rest/v1/ping" -H 'user_key: [your_api-key]'
