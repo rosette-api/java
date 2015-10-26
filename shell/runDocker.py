8b25a3665f06089f2626468fcbc451338255087a
William Spitzer <wspitzer@basistech.com>
2015-10-26 03:24:09 -0400

    added docker files

---
+++ shell/runDocker.py
@@ -0,0 +1,12 @@
+import subprocess
+examples = ["categories", "entities", "entities_linked", "info", "language", "matched-name", "morphology_complete", "morphology_compound-components", "morphology_han-readings", "morphology_lemmas", "morphology_parts-of-speech", "ping", "sentences", "sentiment", "tokens", "translated-name"]
+ext = ".sh"
+process = subprocess.Popen(["mvn", "-Dtarget=true"],shell=True)
+stdoutdata, stderrdata = process.communicate()
+subprocess.call(["docker", "build", "-t", "examples", "."])
+for i in examples:
+    j = i + ext
+    cmd = subprocess.Popen(["docker", "run", "-e", "file=" + j, "-e", "key=88afd6b4b18a11d1248639ecf399903c", "-e", "path=target/github-publish/examples", "examples"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    cmd_out, cmd_err = cmd.communicate()
+    print cmd_out
+    
