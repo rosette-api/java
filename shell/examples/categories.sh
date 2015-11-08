db6bcd7afff35682d8c93c9634d5ec7a4e2032cf
William Spitzer <wspitzer@basistech.com>
2015-11-08 18:17:30 -0500

    Testing categories.sh

--- shell/examples/categories.sh
+++ shell/examples/categories.sh
@@ -1,3 +1,10 @@
+apikey = $1
+
+if [ -z "$apikey" ]; then
+   echo "Must have API key to run example"
+   exit 1
+fi
+
 curl "https://api.rosette.com/rest/v1/categories" \
   -H 'user_key: [your_api-key]' \
   -H 'Content-Type:application/json' \
