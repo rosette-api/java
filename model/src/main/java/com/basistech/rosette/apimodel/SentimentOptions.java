e3b723543f168a2b797a88a15e5aa79414a3ca4b
Benson Margulies <benson@basistech.com>
2016-02-03 10:11:22 -0500

    WS-609 refine.

--- model/src/main/java/com/basistech/rosette/apimodel/SentimentOptions.java
+++ model/src/main/java/com/basistech/rosette/apimodel/SentimentOptions.java
@@ -43,7 +43,9 @@ public final class SentimentOptions {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
