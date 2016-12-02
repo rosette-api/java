3815293bfbb6f60feddb1ee1378feab0bdecad8e
Katsuya-Tomioka <katsuya@basistech.com>
2016-12-02 16:59:06 -0500

    WS-1025: added relationships discoveryMode option (#82)
    * WS-1025: added relationships discoveryMode option
    
    * WS-1025: added accuracy mode in the builder
    
    * WS-1025: fixed equals

--- model/src/main/java/com/basistech/rosette/apimodel/RelationshipsOptions.java
+++ model/src/main/java/com/basistech/rosette/apimodel/RelationshipsOptions.java
@@ -16,12 +16,15 @@
 
 package com.basistech.rosette.apimodel;
 
+import java.util.Objects;
+
 /**
  * Relationship extraction options
  */
 public final class RelationshipsOptions extends Options {
 
     private AccuracyMode accuracyMode;
+    private Boolean discoveryMode;
 
     /**
      * Create a set of relationship extraction options with default values.
@@ -34,10 +37,11 @@ public final class RelationshipsOptions extends Options {
     /**
      * constructor for {@code RelationshipOptions}
      * @param accuracyMode   accuracyMode to use for relationship extraction
+     * @param discoveryMode  discoveryMode to use for relationship extraction
      */
-    public RelationshipsOptions(
-            AccuracyMode accuracyMode) {
+    public RelationshipsOptions(AccuracyMode accuracyMode, Boolean discoveryMode) {
         this.accuracyMode = accuracyMode;
+        this.discoveryMode = discoveryMode;
     }
 
     /**
@@ -56,23 +60,64 @@ public final class RelationshipsOptions extends Options {
         this.accuracyMode = accuracyMode;
     }
 
-    @Override
-    public int hashCode() {
-        return accuracyMode != null ? accuracyMode.hashCode() : 0;
+    /**
+     * @return the discoveryMode flag.
+     */
+    public Boolean getDiscoveryMode() {
+        return discoveryMode;
     }
 
-    /**
-     * if the param is a {@code RelationshipOptions}, compare contents for equality
-     * @param o the object
-     * @return whether or not the param object is equal to this object
-     */
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof RelationshipsOptions)) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-
         RelationshipsOptions that = (RelationshipsOptions) o;
-        return accuracyMode != null ? accuracyMode.equals(that.getAccuracyMode()) : that.accuracyMode == null;
+        return accuracyMode == that.accuracyMode
+            && Objects.equals(discoveryMode, that.discoveryMode);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accuracyMode, discoveryMode);
+    }
+
+    public static class Builder {
+        private Boolean discoveryMode;
+        private AccuracyMode accuracyMode;
+
+        public Builder() {
+            //
+        }
+
+        /**
+         * DocumentRequest accuracy mode for the relationship endpoint.
+         * @param accuracyMode {@link AccuracyMode} to use.
+         * @return this.
+         */
+        public RelationshipsOptions.Builder accuracyMode(AccuracyMode accuracyMode) {
+            this.accuracyMode = accuracyMode;
+            return this;
+        }
+
+
+        /**
+         * DocumentRequest discovery mode. If the value is {@code true}, then the the endpoint will open relationship
+         * extraction results. If {@code false}, not. If {@code null}, the endpoint will perform default processing.
+         * @param discoveryMode discovery mode
+         * @return this.
+         */
+        public RelationshipsOptions.Builder discoveryMode(Boolean discoveryMode) {
+            this.discoveryMode = discoveryMode;
+            return this;
+        }
+
+        public RelationshipsOptions build() {
+            return new RelationshipsOptions(accuracyMode, discoveryMode);
+        }
+    }
+
 }
