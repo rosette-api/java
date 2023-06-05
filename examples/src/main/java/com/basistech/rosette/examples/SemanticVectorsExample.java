109c6cf716c211dbedb7537a04b8cf595711c09e
Julien Cherry <6952800+juliencherry@users.noreply.github.com>
2023-06-05 16:36:07 -0400

    RD-2603: Update examples of semantic vectors and similar terms endpoints

--- examples/src/main/java/com/basistech/rosette/examples/SemanticVectorsExample.java
+++ examples/src/main/java/com/basistech/rosette/examples/SemanticVectorsExample.java
@@ -1,22 +1,23 @@
 /*
-* Copyright 2022 Basis Technology Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2023 Basis Technology Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.basistech.rosette.examples;
 
 import com.basistech.rosette.api.HttpRosetteAPI;
 import com.basistech.rosette.apimodel.DocumentRequest;
+import com.basistech.rosette.apimodel.EmbeddingsMode;
 import com.basistech.rosette.apimodel.SemanticVectorsOptions;
 import com.basistech.rosette.apimodel.SemanticVectorsResponse;
 
@@ -50,6 +51,9 @@ public final class SemanticVectorsExample extends ExampleBase {
         // When no options, use <?>.
         DocumentRequest<SemanticVectorsOptions> request = DocumentRequest.<SemanticVectorsOptions>builder()
             .content(semanticVectorsData)
+            .options(SemanticVectorsOptions.builder()
+                .embeddingsMode(EmbeddingsMode.GEN_1)
+                .build())
             .build();
         SemanticVectorsResponse response = rosetteApi.perform(SEMANTIC_VECTORS_SERVICE_PATH, request, SemanticVectorsResponse.class);
         System.out.println(responseToJson(response));
