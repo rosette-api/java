cd49fba9a930959babc9e730c3f5e6e62a41712d
Fiona Hasanaj <hasanaj.f@husky.neu.edu>
2024-02-13 23:03:11 -0500

    RLPNC-7385: Fixed failing test

--- model/src/main/java/com/basistech/rosette/apimodel/recordsimilarity/deserializers/RecordSimilarityResultDeserializer.java
+++ model/src/main/java/com/basistech/rosette/apimodel/recordsimilarity/deserializers/RecordSimilarityResultDeserializer.java
@@ -39,19 +39,17 @@ public class RecordSimilarityResultDeserializer extends StdDeserializer<RecordSi
 
     @Override
     public RecordSimilarityResult deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        try (jsonParser) {
-            final JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-            final Map<String, RecordSimilarityFieldInfo> fields = node.get("fields").traverse(jsonParser.getCodec()).readValueAs(new TypeReference<Map<String, RecordSimilarityFieldInfo>>() {
-            });
-            final Double score = node.get("score").traverse(jsonParser.getCodec()).readValueAs(Double.class);
-            final Map<String, RecordSimilarityExplainInfo> explainInfo = node.get("explainInfo").traverse(jsonParser.getCodec()).readValueAs(new TypeReference<Map<String, RecordSimilarityExplainInfo>>() {
-            });
-            return RecordSimilarityResult.builder()
-                    .score(score)
-                    .left(parseRecord(node.get("left"), fields, jsonParser))
-                    .right(parseRecord(node.get("right"), fields, jsonParser))
-                    .explainInfo(explainInfo)
-                    .build();
-        }
+        final JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        final Map<String, RecordSimilarityFieldInfo> fields = node.get("fields").traverse(jsonParser.getCodec()).readValueAs(new TypeReference<Map<String, RecordSimilarityFieldInfo>>() {
+        });
+        final Double score = node.get("score").traverse(jsonParser.getCodec()).readValueAs(Double.class);
+        final Map<String, RecordSimilarityExplainInfo> explainInfo = node.get("explainInfo").traverse(jsonParser.getCodec()).readValueAs(new TypeReference<Map<String, RecordSimilarityExplainInfo>>() {
+        });
+        return RecordSimilarityResult.builder()
+                .score(score)
+                .left(parseRecord(node.get("left"), fields, jsonParser))
+                .right(parseRecord(node.get("right"), fields, jsonParser))
+                .explainInfo(explainInfo)
+                .build();
     }
 }
