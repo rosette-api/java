2cdf95c82732cdef1045a6786f7f71799ed04335
Grace Calianese <gcalianese@babelstreet.com>
2024-04-16 15:53:06 -0400

    update UnknownField deserializer

--- json/src/main/java/com/basistech/rosette/apimodel/jackson/recordsimilaritydeserializers/UnknownFieldDeserializer.java
+++ json/src/main/java/com/basistech/rosette/apimodel/jackson/recordsimilaritydeserializers/UnknownFieldDeserializer.java
@@ -20,7 +20,7 @@ import com.basistech.rosette.apimodel.recordsimilarity.records.UnknownField;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-//import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
@@ -32,16 +32,12 @@ public class UnknownFieldDeserializer extends StdDeserializer<UnknownField> {
 
     @Override
     public UnknownField deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-//        final JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-        try {
-            return UnknownField.builder().build();
-//        if (node.isObject()) {
-//            return jsonParser.getCodec().treeToValue(node, AddressField.FieldedAddress.class);
-//        } else if (node.isTextual()) {
-//            return AddressField.UnfieldedAddress.builder().address(node.textValue()).build();
-//        }
-        } catch (Exception e) {
-            throw new IOException("Invalid JSON structure: unexpected node type");
+        final JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        if (node.isObject()) {
+            return jsonParser.getCodec().treeToValue(node, UnknownField.class);
+        } else if (node.isTextual()) {
+            return new UnknownField(node.textValue());
         }
+        throw new IOException("Invalid JSON structure: unexpected node type");
     }
 }
