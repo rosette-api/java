cf54b25550635dd120e735d7126d7b0e50167191
jsquatrito <jsquatrito@babelstreet.com>
2024-05-17 12:59:38 -0400

    RLPNC-7496: Removed unneeded validation/enforcement, added getter

--- model/src/main/java/com/basistech/rosette/apimodel/recordsimilarity/RecordSimilarityFieldInfo.java
+++ model/src/main/java/com/basistech/rosette/apimodel/recordsimilarity/RecordSimilarityFieldInfo.java
@@ -19,6 +19,7 @@ package com.basistech.rosette.apimodel.recordsimilarity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import javax.validation.Valid;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
@@ -29,7 +30,7 @@ import javax.validation.constraints.NotNull;
 @Value
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RecordSimilarityFieldInfo {
-    @NotNull @Valid String type;
+    @Getter @NotNull @Valid String type;
     Double weight;
     /**
      * the score that this field should return if the field is null in a record
